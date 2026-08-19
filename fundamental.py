be572349b9fb4f1999c93cce12f1186ffd0908db
V Bramhaji <vbbrahmaji@gmail.com>
2026-08-19 12:23:50 +0530

    Add files via upload

---
+++ fundamental.py
@@ -0,0 +1,269 @@
+# # #printstatememnt:
+# # #print(expression)
+# # print("hello")
+# # print("i am python developer")
+
+# # # comments
+# # # explain the code to understand to other PermissionError
+
+# # # veriables
+# # '''
+# # veriable is container to store the data values
+# # veriable is set of rules to followw in programming
+# # 1 vreable is start with letter and underscore character
+# # 2 veriable is not start with number
+# # 3 veriavle is name case sensitive
+# # 4 veraible is not use keywords
+
+# # '''
+# # x=23
+# # print(x)
+# # x="hello"
+# # print(x)
+# # x=23.4
+# # print(x)
+# # myname="hello"
+# # print(myname)
+# # #invalid syntax
+# # # my name="helo"
+# # # print(my name) #invalid syntax
+
+# # # veraible types
+# # # camel case : each world start with one letter capital
+# # myName="brahmi"
+# # print(myName)
+# # # sanke case : each word space to undeescope
+# # my_name="hello"
+# # print(my_name)
+# # # pascel case:each word start with capital letter
+# # MyName="hello"
+# # print("MyName")
+# # # data types
+# # x=4
+# # print(x)
+# # print(type(x))
+# # x=34.5
+# # print(x)
+# # print(type(x))
+# # x="string"
+# # print(x)
+# # print(type(x))
+
+# # # type conversion
+
+# # # two type :1 implicit type 2 explicit convesion
+
+# # x=2
+# # y=34.5
+# # c=x+y
+# # print(c)
+# # print(type(c))
+
+# # x=34
+# # y=float(x)
+# # print(y)
+# # x=23
+# # y=str(x)
+# # print(y)
+# # x=2.3
+# # y=int(x)
+# # print(y)
+
+# # x=2.3
+# # y=str(x)
+# # print(y)
+# # print(type(y))
+
+# # x="234"
+# # y=int(x)
+# # print(y)
+# # # x="hello"
+# # y=int(x)
+
+# # print(y)
+
+# # sample_list=[23,34,"pythonLife"]
+# # print(sample_list)
+
+# # list=[23,34.4,"python",[23,34.3,"python"]]
+# # print(list)
+# # print(type(list))
+
+# # tuple=(23,24,34.4,"python",(23.23),[23,4,5])
+# # print(tuple)
+# # print(type(tuple))
+
+# # set={23,34.4,"python",(23,34)}
+# # print(set)
+
+# # dict={"name":"brahmi","age":30,"class":9}
+# # print(dict)
+# # d3=dict.keys()
+# # print(d3)
+# # d4=dict.values()
+# # print(d4)
+# # dict["name"]="nani"
+# # print(dict)
+# # dict["gender"]="male"
+# # print(dict)
+# # dict.pop("gender")
+# # dict.popitem()
+# # dict.pop("age")
+# # del dict["name"]
+# # print(dict)
+# # dict={"name":"brahmi","age":30,"class":9}
+# # dict.clear()
+# # print(dict)
+# # dict={"name":"brahmi","age":30,"class":9}
+# # dict.update({"color":"red"})
+# # dict["gender"]="male"
+# # print(dict)
+# # for x in dict.items():
+
+# #     print(x)
+# # for x in dict.values():
+# #     print(x)
+# # dict={"name":"brahmi","age":30,"class":9}
+# # child={"student":"brahmi","age":35 }
+# # child3={"class":"34","student":"nami"}
+# # myfamily={"dict":dict,"child":child,"child3":child3}
+# # print(myfamily["child"])
+
+# # list=[23,34,34.3,"python"]
+# # print(list)
+# # list.append(23)
+# # list.pop(2)
+# # list.remove("python")
+# # list.extend([23,24])
+# # list.clear()
+# # list.insert(0,2)
+# # list.index(2)
+# # # print(list)
+# # # my_list=[23,34,34.3,"python",34]
+# # # list2=set(my_list)
+# # # print(list2)
+
+# my_list=[23,23.4,45,"python",23]
+# my_set=list(dict.fromkeys(my_list))
+
+# print(my_set)
+
+# my_list=[23,23.4,45,"python",23]
+# new_list=tuple(my_list)
+# new_list2=set(my_list)
+# print(new_list2)
+
+
+# array=[87,7,56,]
+# print(type(array))
+
+# my_list=[12,12.3,34,"python"]
+
+# list2=list(dict.fromkeys(my_list))
+# print(list2)
+
+
+# printstatement syntax
+# print(10+10)
+# print("string")
+# print(1,2,3,4)
+# print([1,2,3,4])
+# print((1,2,3,4,5))
+# print({1,32,3})
+
+# # variable
+# x=23
+# print(x)
+
+# x=c=v=23
+# print(x)
+# print(c)
+# print(v)
+# print(v)
+# x,c,f=23,23,23
+# print(x)
+# print(c)
+# print(f)
+
+# variable roles
+
+# Stuent_name={1,2,3} #snake
+# StudentName=[1,2,3,4]#camel
+# student=(1,2,33,4)#pascal
+# print(Stuent_name)
+# print(StudentName)
+
+# operator
+# int
+# student_age=int(input("enter the age"))
+# print(student_age)
+
+
+# float
+# student_age=float(input("enter the age"))
+# print(student_age)
+
+
+# student_age=str(input("enter the age"))
+# print(student_age)
+
+# student_age=bool(input("enter the age"))
+# print(student_age)
+
+# type conversion
+
+# int to float
+# student_age= int(input("enter the age"))
+# student_age2=float(student_age)
+# print(student_age2)
+
+# student_age= int(input("enter the age"))
+# student_age2=str(student_age)
+# print(student_age2)
+
+                #    float to int
+# student_age= float(input("enter the age"))
+# student_age2=int(student_age)
+# print(student_age2)
+
+
+# student_age= float(input("enter the age"))
+# student_age2=str(student_age)
+# print(student_age2)
+
+# student_age= str(input("enter the age"))
+# student_age2=float(student_age)
+# print(student_age2)
+
+
+# student_list=["name","age","branch","hello"]
+# print(student_list)
+# print(type(student_list))
+
+# student_tuple=(1,2,3,"python",(12,34,5))
+# print(student_tuple)
+
+# student_set={1,2,3,4,"python",(1,3,4,5,6)}
+# print(student_set)
+
+# student_dict={"name":"brahmi","age":23,"branch":"cse"}
+
+# student_dict["gender"]="male"
+# print(student_dict.keys())
+# print(student_dict.values())
+# print(student_dict.items())
+# del student_dict['branch']
+# student_dict.pop("age")
+# sample=student_dict.copy()
+# print(student_dict)
+# print(sample)
+# sample.clear()
+# print(sample)
+
+list=[12,12,12,3,3,44,4]
+empy=[]
+for i in list:
+    if i%2==0:
+        # print()
+        empy.append(i)
+print(empy)
